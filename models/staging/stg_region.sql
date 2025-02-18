36ac731ceedbab15d6d01a266f74a0e6f1841433
sai sri <snowflaketraining48@gmail.com>
2025-02-18 05:55:18 +0000

    commit

--- models/staging/stg_region.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-WITH tb1 as(
-    select 
-    R_REGIONKEY AS REGIONKEY,
-	R_NAME AS NAME,
-	R_COMMENT AS COMMENT
-    from {{source('datafeed_shared_schema','RAW_REGION')}})
-select * from tb1
