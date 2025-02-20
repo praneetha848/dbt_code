4c1322f0eb7688230d1f8b8906b3a28806e2583c
sai sri <snowflaketraining48@gmail.com>
2025-02-20 03:50:42 +0000

    commit

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,9 +1,9 @@
-{{ config(materialized='table') }}
+{{ config(materialized='table',transient=false) }}
 
 WITH tb1 as(
     select id,
     order_date,
     user_id,
     status
-    from {{source('datafeed_shared_schema','raw_order')}})
+    from {{source('datafeed_shared_schema','raw_order')}}
 select * from tb1
