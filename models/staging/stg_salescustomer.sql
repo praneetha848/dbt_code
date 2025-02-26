6ce7c36cafe7ca6841eea9340281d5c9ed3a37db
naveenyerawar1@gmail.com <naveenyerawar1@gmail.com>
2025-02-26 03:33:54 +0000

    commit

---
+++ models/staging/stg_salescustomer.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+WITH tb1 as(
+    select
+    c_custkey as customer_key, 
+    c_name as customer_name, 
+    c_nationkey as nation_key    
+
+    from {{source('datafeed_shared_schema_sales','raw_customer')}})
+select * from tb1
