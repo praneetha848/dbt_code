d8bed91f489abcb4a48259b9becd5db33b196b55
sai sri <snowflaketraining48@gmail.com>
2025-03-24 13:25:21 +0000

    commit

--- models/example/pre_post_model.sql
+++ models/example/pre_post_model.sql
@@ -1,6 +1,6 @@
 {{ config(
 materialized="table",
-
+pre_hook="TRUNCATE TABLE {{ this }}",
 ) }}
 
 WITH tb1 as(
@@ -8,3 +8,5 @@ select
  *
  from {{source('datafeed_shared_schema','raw_payments')}})
 select * from tb1
+
+
